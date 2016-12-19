ec8d7092dc259801c1558b1d0e60c13a212fc47c
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2016-12-19 07:03:52 +0200

    Add project files.

---
+++ AssetManagement3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssetManagement3.Startup))]
+namespace AssetManagement3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
