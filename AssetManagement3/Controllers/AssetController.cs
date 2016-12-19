9fdd12deea9ff19cddcc2e521e61c3294e0cca0d
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2016-12-19 08:58:13 +0200

    Lisätty yksinkertainen toteutus laitteiden tallentamisesta tiettyyn paikkaan.

---
+++ AssetManagement3/Controllers/AssetController.cs
@@ -0,0 +1,149 @@
+﻿using AssetManagement3.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using AssetManagement3.Models;
+using AssetManagement3.Database;
+
+namespace AssetManagement3.Controllers
+{
+    public class AssetController : Controller
+    {
+        // GET: Asset
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Asset/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asset/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AssignLocation()
+        {
+            string json = Request.InputStream.ReadToEnd();
+            AssignLocatioModel inputData =
+                JsonConvert.DeserializeObject<AssignLocatioModel>(json);
+            bool success = false;
+            string error = "";
+
+            Omat_tietokannatEntities entities = new Omat_tietokannatEntities();
+            try
+            {
+
+                int locationId = (from l in entities.AssetLocations
+                                  where l.Code == inputData.LocationCode
+                                  select l.Id).FirstOrDefault();
+
+                int assetId = (from a in entities.AssetLocations
+                               where a.Code == inputData.LocationCode
+                               select a.Id).FirstOrDefault();
+
+                if ((locationId > 0) && (assetId = > 0))
+
+                {
+                    //tallennetaan uusirivi aikaleiman kanssa kantaa.
+                    AssetLocation1 newEntry = new AssetLocation1();
+                    newEntry.LocationId = locationId;
+                    newEntry.AssetId = assetId;
+                    newEntry.LastSeen = DateTime.Now;
+
+                    entities.AssetLocations1.Add(newEntry);
+                    entities.SaveChanges();
+                    success = true;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex.GetType().Name + ": " + ex.Message;
+            }          
+            finally 
+            {
+                entities.Dispose();
+            }
+            var result = new { success = success, error = error };
+            return Json(result);
+
+            
+
+
+            }
+        }
+
+
+
+        // POST: Asset/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AssetManagement3/Models/AssignLocatioModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssetManagement3.Models
+{
+    public class AssignLocatioModel
+    {
+        public string AssetCode { get; set; }
+
+        public string LocationCode { get; set; }
+
+    }
+}
